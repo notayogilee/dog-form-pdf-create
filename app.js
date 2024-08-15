dd84c25baa259df6c25265b4b5ab3afc9146f35e
notayogilee <notayogilee@gmail.com>
2024-08-15 10:35:07 -0700

    Format the pdf file output.

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ let month = months[today.getMonth()];
 let year = today.getFullYear();
 
 let currentDate = `${day} ${month}, ${year}`;
+const dateForPDFName = `${day}${month.slice(0, 3).toUpperCase()}${year}`;
 
 let datePlaceholder = document.getElementById("current-date");
 datePlaceholder.innerText = currentDate;
@@ -59,7 +60,6 @@ function validateInput(event) {
 }
 
 // Create and download pdf
-
 const createPDF = () => {
   const clientName = document.getElementById("clientName").value;
   const clientPhone = document.getElementById("clientPhone").value;
@@ -69,9 +69,17 @@ const createPDF = () => {
   let newName = document.getElementById("newName").value;
   if (!newName) newName = animatchName;
   const dateAdopted = document.getElementById("dateAdopted").value;
-  const issues = document.getElementById("issues").value;
-  const tried = document.getElementById("tried").value;
-  const recommendations = document.getElementById("recommendations").value;
+  const yearAdopted = dateAdopted.slice(0, 4);
+  const monthAdopted = dateAdopted.slice(6, 8);
+  const dayAdopted = dateAdopted.slice(-2);
+  const formattedDateAdopted = `${dayAdopted} ${
+    months[parseInt(monthAdopted) - 1]
+  }, ${yearAdopted}`;
+  const issues = document.getElementById("issues").value.trim();
+  const tried = document.getElementById("tried").value.trim();
+  const recommendations = document
+    .getElementById("recommendations")
+    .value.trim();
 
   const followUpPhone = document.getElementById("phone").checked;
   const followUpEmail = document.getElementById("email").checked;
@@ -87,6 +95,7 @@ const createPDF = () => {
   const doc = new jsPDF("portrait", "mm", "a4");
 
   let y = 20;
+
   const pageHeight = doc.internal.pageSize.height;
 
   const addTextToPDF = (text, x, y) => {
@@ -103,44 +112,54 @@ const createPDF = () => {
     doc.text(textLines, x, y);
     return blockHeight;
   };
-
+  doc.setFontSize(20);
   doc.text("Client", 20, y);
   doc.text(`Date: ${currentDate}`, 130, y);
+  y += 7;
 
-  y += 10;
-
+  doc.setFontSize(14);
   doc.text(`Name: ${clientName}`, 20, y);
   y += 7;
   doc.text(`Phone: ${clientPhone}`, 20, y);
   y += 7;
   doc.text(`Email: ${clientEmail}`, 20, y);
-  y += 10;
+  y += 15;
 
+  doc.setFontSize(20);
   doc.text("Dog Info", 20, y);
-  y += 10;
+  y += 7;
+  doc.setFontSize(14);
   doc.text(`Dog Name (Animatch given): ${animatchName}`, 20, y);
   y += 7;
   doc.text(`New Dog Name (owner given): ${newName}`, 20, y);
   y += 7;
-  doc.text(`Date Adopted: ${dateAdopted}`, 20, y);
+  doc.text(`Date Adopted: ${formattedDateAdopted}`, 20, y);
   y += 10;
+  doc.setFontSize(20);
   doc.text("Issues", 20, y);
   y += 7;
+  doc.setFontSize(14);
   y += addTextToPDF(issues, 20, y);
-  y += 10;
+  y += 5;
 
+  doc.setFontSize(20);
   doc.text("Tried", 20, y);
   y += 7;
+  doc.setFontSize(14);
   y += addTextToPDF(tried, 20, y);
+  y += 5;
 
+  doc.setFontSize(20);
   doc.text("Recommendations", 20, y);
   y += 7;
+  doc.setFontSize(14);
   y += addTextToPDF(recommendations, 20, y);
-  y += 10;
+  y += 5;
 
+  doc.setFontSize(18);
   y += addTextToPDF(`Follow up by ${followUp}`, 20, y);
 
-  doc.save(`${newName.replaceAll(" ", "-")}.pdf`);
+  doc.save(`${newName.replaceAll(" ", "-")}-${dateForPDFName}.pdf`);
 
   // doc.html(html, {
   //   callback: function () {
