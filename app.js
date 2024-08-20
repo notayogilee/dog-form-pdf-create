40e1c5f50dead9010a41f09272f336c7a0eabfc9
notayogilee <notayogilee@gmail.com>
2024-08-19 19:13:55 -0700

    Add new form fields. Change pdf form to contain editable fields.

--- app.js
+++ app.js
@@ -68,6 +68,7 @@ const createPDF = () => {
   const clientName = document.getElementById("clientName").value;
   const clientPhone = document.getElementById("clientPhone").value;
   const clientEmail = document.getElementById("clientEmail").value;
+  const clientAddress = document.getElementById("clientAddress").value;
 
   const animatchName = document.getElementById("animatchName").value;
   let newName = document.getElementById("newName").value;
@@ -84,6 +85,7 @@ const createPDF = () => {
   const recommendations = document
     .getElementById("recommendations")
     .value.trim();
+  const notes = document.getElementById("notes").value.trim();
 
   const followUpPhone = document.getElementById("phone").checked;
   const followUpEmail = document.getElementById("email").checked;
@@ -119,58 +121,131 @@ const createPDF = () => {
   doc.setFontSize(20);
   doc.text("Client", 20, y);
   doc.text(`Date: ${currentDate}`, 130, y);
-  y += 7;
+  y += 10;
 
   doc.setFontSize(14);
-  doc.text(`Name: ${clientName}`, 20, y);
-  y += 7;
-  doc.text(`Phone: ${clientPhone}`, 20, y);
-  y += 7;
-  doc.text(`Email: ${clientEmail}`, 20, y);
+
+  doc.text(`Name:`, 20, y);
+
+  let textField = new doc.AcroForm.TextField();
+  textField.Rect = [35, y - 5, 100, y + 5];
+  textField.height = 7;
+  textField.width = 150;
+  textField.backgroundColor = "grey";
+  textField.fileSelect - true;
+  textField.value = clientName;
+  doc.addField(textField);
+  y += 10;
+
+  doc.text(`Phone:`, 20, y);
+
+  let phoneField = new doc.AcroForm.TextField();
+  phoneField.Rect = [37, y - 5, 100, y + 5];
+  phoneField.height = 7;
+  phoneField.width = 40;
+  phoneField.backgroundColor = "grey";
+  phoneField.value = clientPhone;
+  doc.addField(phoneField);
+
+  doc.text(`Email:`, 80, y);
+  let emailField = new doc.AcroForm.TextField();
+  emailField.Rect = [95, y - 5, 100, y + 5];
+  emailField.height = 7;
+  emailField.width = 90;
+  emailField.backgroundColor = "grey";
+  emailField.value = clientEmail;
+  doc.addField(emailField);
+  y += 10;
+
+  doc.text(`Address:`, 20, y);
+
+  let addressField = new doc.AcroForm.TextField();
+  addressField.Rect = [40, y - 5, 100, y + 5];
+  addressField.height = 7;
+  addressField.width = 145;
+  addressField.backgroundColor = "grey";
+  addressField.value = clientAddress;
+  doc.addField(addressField);
   y += 15;
 
   doc.setFontSize(20);
   doc.text("Dog Info", 20, y);
   y += 7;
   doc.setFontSize(14);
-  doc.text(`Dog Name (Animatch given): ${animatchName}`, 20, y);
+  doc.text(`Animatch Name: ${animatchName}`, 20, y);
+
+  doc.text(`Given Name:`, 100, y);
+
+  let newDogNameField = new doc.AcroForm.TextField();
+  newDogNameField.Rect = [130, y - 5, 100, y + 5];
+  newDogNameField.height = 7;
+  newDogNameField.width = 60;
+  newDogNameField.backgroundColor = "grey";
+  newDogNameField.value = newName;
+  doc.addField(newDogNameField);
   y += 7;
-  doc.text(`New Dog Name (owner given): ${newName}`, 20, y);
-  y += 7;
+
   doc.text(`Date Adopted: ${formattedDateAdopted}`, 20, y);
   y += 10;
   doc.setFontSize(20);
   doc.text("Issues", 20, y);
-  y += 7;
-  doc.setFontSize(14);
-  y += addTextToPDF(issues, 20, y);
   y += 5;
+  doc.setFontSize(14);
+  let issuesField = new doc.AcroForm.TextField();
+  issuesField.Rect = [20, y, 180, y + 10];
+  issuesField.height = 30;
+  issuesField.width = 180;
+  issuesField.backgroundColor = "grey";
+  issuesField.value = issues;
+  issuesField.multiline = true;
+  doc.addField(issuesField);
+  y += 40;
 
   doc.setFontSize(20);
   doc.text("Tried", 20, y);
-  y += 7;
-  doc.setFontSize(14);
-  y += addTextToPDF(tried, 20, y);
   y += 5;
+  doc.setFontSize(14);
+  let triedField = new doc.AcroForm.TextField();
+  triedField.Rect = [20, y, 180, y + 10];
+  triedField.height = 30;
+  triedField.width = 180;
+  triedField.backgroundColor = "grey";
+  triedField.value = tried;
+  triedField.multiline = true;
+  doc.addField(triedField);
+  y += 40;
 
   doc.setFontSize(20);
   doc.text("Recommendations", 20, y);
-  y += 7;
-  doc.setFontSize(14);
-  y += addTextToPDF(recommendations, 20, y);
   y += 5;
+  doc.setFontSize(14);
+  let recommendationsField = new doc.AcroForm.TextField();
+  recommendationsField.Rect = [20, y, 180, y + 10];
+  recommendationsField.height = 30;
+  recommendationsField.width = 180;
+  recommendationsField.backgroundColor = "grey";
+  recommendationsField.value = recommendations;
+  recommendationsField.multiline = true;
+  doc.addField(recommendationsField);
+  y += 40;
+
+  doc.setFontSize(20);
+  doc.text("Notes", 20, y);
+  y += 5;
+  doc.setFontSize(14);
+  let notesField = new doc.AcroForm.TextField();
+  notesField.Rect = [20, y, 180, y];
+  notesField.height = 30;
+  notesField.width = 180;
+  notesField.backgroundColor = "grey";
+  notesField.value = notes;
+  notesField.multiline = true;
+  console.log(notesField);
+  doc.addField(notesField);
+  y += 40;
 
   doc.setFontSize(18);
   y += addTextToPDF(`Follow up by ${followUp}`, 20, y);
 
   doc.save(`${newName.replaceAll(" ", "-")}-${dateForPDFName}.pdf`);
-
-  // doc.html(html, {
-  //   callback: function () {
-  //     doc.save("Lee-sample-file.pdf");
-  //   },
-  //   x: 20,
-  //   y: 20,
-  //   width: 170,
-  // });
 };
